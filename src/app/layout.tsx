2d555f370384105fb87bb32b5e8fd59ac465a22a
ram-prasad-1 <ramprasaddeora@gmail.com>
2024-06-19 18:16:52 +0530

    few changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,7 @@ import { cn } from "@/lib/utils"
 import { SiteHeader } from "@/components/site-header"
 import { TailwindIndicator } from "@/components/tailwind-indicator"
 import { ThemeProvider } from "@/components/theme-provider"
+import { Toaster } from "@/components/ui/toaster"
 
 export const metadata: Metadata = {
   title: {
@@ -33,21 +34,22 @@ export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <>
       <html lang="en" suppressHydrationWarning>
-        <head />
-        <body
-          className={cn(
-            "min-h-screen bg-background font-sans antialiased",
-            fontSans.variable
-          )}
-        >
-          <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-            <div className="relative flex min-h-screen flex-col">
-              <SiteHeader />
-              <div className="flex-1">{children}</div>
-            </div>
-            <TailwindIndicator />
-          </ThemeProvider>
-        </body>
+      <head />
+      <body
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          fontSans.variable
+        )}
+      >
+      <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+        <div className="relative flex min-h-screen flex-col">
+          <SiteHeader />
+          <div className="flex-1">{children}</div>
+        </div>
+        <Toaster />
+        <TailwindIndicator />
+      </ThemeProvider>
+      </body>
       </html>
     </>
   )
---
+++ src/app/testComponents/page.tsx
@@ -0,0 +1,13 @@
+import {InputForm} from "@/allTestComponents/InputDisable";
+
+
+export default function Page () {
+
+  return (
+    <div className="p-10">
+      <h2> Input with disable state:</h2>
+      <InputForm />
+    </div>
+
+  )
+}
